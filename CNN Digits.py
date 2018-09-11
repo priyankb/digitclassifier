7747fb212b7f348dcae05b00fc7f896cdaa3b12f
priyankb <priyankbathula@gmail.com>
2018-09-11 14:31:13 -0700

    added source code

---
+++ CNN Digits.py
@@ -0,0 +1,54 @@
+
+# coding: utf-8
+
+# In[21]:
+
+
+import tensorflow as tf
+
+Sequential = tf.keras.models.Sequential
+Dense = tf.keras.layers.Dense
+Flatten = tf.keras.layers.Flatten
+Conv2D = tf.keras.layers.Conv2D
+MaxPooling2D = tf.keras.layers.MaxPooling2D
+
+mnist = tf.keras.datasets.mnist
+
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+img_x, img_y = 28, 28
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 1)
+x_test = x_test.reshape(x_test.shape[0], img_x, img_y, 1)
+input_shape = (img_x, img_y, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(optimizer='Adam',
+              loss=tf.keras.losses.categorical_crossentropy,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss: ', score[0])
+print('Test accuracy: ', score[1])
+
