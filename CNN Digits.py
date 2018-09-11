6b333f863995087a4b89d36f0b3ae39b11733e1d
priyankb <priyankbathula@gmail.com>
2018-09-11 14:35:07 -0700

    changes made to source

--- CNN Digits.py
+++ CNN Digits.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[21]:
-
-
 import tensorflow as tf
 
 Sequential = tf.keras.models.Sequential
@@ -34,14 +28,14 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu',
+                 activation=tf.nn.relu,
                  input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(Conv2D(64, (5, 5), activation=tf.nn.relu))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(1000, activation=tf.nn.relu))
+model.add(Dense(num_classes, activation=tf.nn.softmax))
 
 model.compile(optimizer='Adam',
               loss=tf.keras.losses.categorical_crossentropy,
@@ -51,4 +45,3 @@ model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, val
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss: ', score[0])
 print('Test accuracy: ', score[1])
-
